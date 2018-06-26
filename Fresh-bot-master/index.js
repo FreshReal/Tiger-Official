34cffcb09bde90c157c287a691990bd5a6561f9a
FreshReal <40603946+FreshReal@users.noreply.github.com>
2018-06-26 18:05:40 +0300

    Add files via upload

---
+++ Fresh-bot-master/index.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+	console.log('SUNT GATA!');
+});
+
+client.on('message', message => { //aici este difinirea la message
+
+    if (message.content === 'ping') {
+        message.channel.send(`PONG`)
+    }
+
+if (message.content.toLowerCase().includes('https://discord.gg')) { //mesajul detectat este https://discord.gg
+    const culoare = new Discord.RichEmbed() //embedul este definit ca culoare
+    if(!message.guild.member(client.user).hasPermission(`MANAGE_MESSAGES`)) return //daca autorul are acces la sters mesaje botul trece peste si nu sterge mesajul
+    //message.channel.send('Nu am acces sa sterg mesajul!')
+    if(message.author.bot) return //daca un bot face reclama botul respectiv trece peste si nu sterge mesajul
+    if (message.author.id === message.guild.owner.user.id) return; //ownershipul are acces la tot, deci are acces si al sters mesaje deci nu o sa ii fie sters mesajul
+    message.delete() //definirea prin care botul sterge mesajul
+    culoare.addField('Reclama?', `${message.author} nu ai voie sa promovezi servere pe aici!`) //mesajul dupa ce a sters reclama
+    culoare.setColor(`ff0000`) //culoarea
+    message.channel.send(culoare); //trimite mesajul
+}
+}); //si aici se incheie, tot ce e dupa }); nu mai este definit, uite un exemplu
+
+client.login('token')
